5fb2b9d22d7f699c0dd9eb2e02572471fe2e5f96
Mariama Tunkara <119751878+mariama2244@users.noreply.github.com>
2023-04-16 15:29:47 +0200

    controller + index + Formtype
    css

---
+++ src/Form/CommentaryFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentary;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaryFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('comment')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('deletedAt')
+            ->add('article')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentary::class,
+        ]);
+    }
+}
