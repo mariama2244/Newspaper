05fc05a5936876051c8b0302cb1d84d6b02fbd6b
Mariama Tunkara <119751878+mariama2244@users.noreply.github.com>
2023-04-17 09:34:11 +0200

    Show article + show home + default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
    #[Route('/', name: 'show_home', methods: ['GET'])]
-   public function showHome(): Response
+   public function showHome(ArticleRepository $repository): Response
    {
-    return $this->render('default/show_home.html.twig');
+      $articles = $repository->findBy(['deletedAt' => null]);
+
+    return $this->render('default/show_home.html.twig', [
+      'article' => $articles 
+    ]);
    } // end function showHome
-}
+}// end class
